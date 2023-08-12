fc4721a7402938fd7edd43686ebf1409d0ca8c33
kaymatic <kaymatic@gmail.com>
2023-08-11 23:33:51 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,20 +5,15 @@
  *
  * Return: always 0
  */
-
 int main(void)
 {
 	int i;
+	char j;
 
 	for (i = 0 ; i < 10 ; i++)
-	{
 		putchar(i + '0');
-		if (i != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
